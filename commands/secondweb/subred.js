85cb2aecab748eb754c1cd2766b1e87cb35e96e1
Ashikur17 <ashikurahman2007@gmail.com>
2025-12-05 19:44:03 +0630

    Update subred.js

--- commands/secondweb/subred.js
+++ commands/secondweb/subred.js
@@ -1,1 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
 
+module.exports = { 
+  data: new SlashCommandBuilder()
+    .setname('subreddit')
+    .setDescription('Get the Link to the r/Alevel subreddit!'),
+  asynx ecute(interaction) {
+    await interaction.reply('Heres the r/Alevel subreddit: https://www.reddit.com/r/alevel/')
+  ),
+);
