b8242f903b82a705a4129f96acfd1473df3f8913
Ashikur17 <ashikurahman2007@gmail.com>
2025-12-10 22:35:00 +0630

    Update task.js

--- commands/task/task.js
+++ commands/task/task.js
@@ -23,8 +23,8 @@ module.exports = {
         await interaction.deferReply({ flags: 64 });
 
         const CHANNEL_TEAMS = {
-            "1448189002057257093": "graphic",
-            "1448189025491091597": "dev",
+            [process.env.GRAPHIC_CHANNEL_ID]: "graphic",
+            [process.env.DEV_CHANNEL_ID]: "dev",
         };
 
         const team = CHANNEL_TEAMS[interaction.channelId];
@@ -101,6 +101,7 @@ module.exports = {
         });
     }
 };
+
 
 
 
